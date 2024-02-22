3235e4d9a0b100523b6482c1ef66e925cc68d678
Kawoogie <85772341+Kawoogie@users.noreply.github.com>
2024-02-22 16:12:33 -0500

    Add files via upload

---
+++ MAXM86161.cpp
@@ -0,0 +1,204 @@
+#include "MAXM86161.h"
+
+/*****************************************************************************/
+// Constructor
+/*****************************************************************************/
+/*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*/
+MAXM86161::MAXM86161(I2C &i2c)
+i2cbus = i2c
+{
+    // Empty block
+}
+
+MAXM86161::~MAXM86161(void){
+    // Empty block
+}
+
+
+// Initialize the sensor
+// Sets the PPG sensor to starting condition, then puts it in SHDN mode ready to
+// take data
+int MAXM86161::init(void)
+{
+    // Use function to do software reset
+    _write_to_reg(REG_SYSTEM_CONTROL, 0x09);
+
+    ThisThread::sleep_for(1ms);
+
+    // Shut Down
+    _write_to_reg(REG_SYSTEM_CONTROL, 0x0A);
+
+    ThisThread::sleep_for(2ms);
+
+    // Clear Interrupt 1 by reading
+    _read_from_reg(REG_IRQ_STATUS1);
+
+    // Clear Interrupt 2 by reading
+    _read_from_reg(REG_IRQ_STATUS2);
+
+    // Set integration time and ADC range with ALC and ADD
+    _write_to_reg(REG_PPG_CONFIG1, 0x0F);
+
+    // Set sample rate and averaging
+    // cmd[0] = 0x12; cmd[1] = 0x50;  // 8Hz with no averaging
+    // cmd[1] = 0x08; 50 Hz with no averaging
+    _write_to_reg(REG_PPG_CONFIG2, 0x08);
+
+    // Set LED settling, digital filter, burst rate, burst enable
+    //  No Burst mode with default settling
+    _write_to_reg(REG_PPG_CONFIG3, 0x40);
+
+    // Set Photodiode bias to 0pF to 65pF
+    _write_to_reg(REG_PD_BIAS, 0x40);
+
+    // Set LED driver range to 124 mA
+    _write_to_reg(REG_LED_RANGE1, 0x3F);
+
+    // Set LED current
+    _write_to_reg(REG_LED1_PA, 0x14);
+    _write_to_reg(REG_LED2_PA, 0x14);
+    _write_to_reg(REG_LED3_PA, 0x14);
+
+    // Enable Low Power Mode
+    _write_to_reg(REG_SYSTEM_CONTROL, 0xC);
+
+    //********************************
+    // FIFO enable
+
+    // Set FIFO full to 15 empty spaces left
+    _write_to_reg(REG_FIFO_CONFIG1, 0xF);
+
+    // Enable FIFO rollover when full
+    _write_to_reg(REG_FIFO_CONFIG2, 0x2);
+
+    // Enable FIFO interrupt
+    _write_to_reg(REG_IRQ_ENABLE1, 0x80);
+
+    // Set LED exposure to timeslots
+    _write_to_reg(REG_LED_SEQ1, 0x43); //LED2 to time slot 1 and LED3 to time slot 2
+    _write_to_reg(REG_LED_SEQ2, 0x00);
+    _write_to_reg(REG_LED_SEQ3, 0x00);
+
+    // Shutdown at the end and wait for signal to start
+    _write_to_reg(REG_SYSTEM_CONTROL, 0b00001110);
+
+    // Read device ID, if it matches the value for MAXM86161, return 0, otherwise return 1.
+
+    // TODO: Write a function to test if the sensor can be communicated with
+    return 0;
+}
+
+int MAXM86161::start(void)
+{
+    
+    // Shutdown at the end and wait for signal to start
+    // TODO: Get value of register
+
+    // Apply mask
+
+    // Write to the register
+    _write_to_reg(REG_SYSTEM_CONTROL, 0b00001100);
+    return 0;
+}
+
+int MAXM86161::stop(void)
+{
+    // Shutdown at the end and wait for signal to start
+    _write_to_reg(REG_SYSTEM_CONTROL, 0b00001110);
+    return 0;
+}
+
+// Function to read from a registry
+int MAXM86161::_read_from_reg(int address, int* data){
+    char cmd[16];
+    char rsp[256];
+    int status;
+    cmd[0] = address;
+    status = i2cbus.write(SH_ADDR, cmd, 1, true);
+    if(status !=0) {
+        serial_pc.printf("Failed to write register address %#X during read.\n", address);
+        return status;
+        }
+    
+    status = i2cbus.read(PPG_ADDR, rsp, 1);
+    if(status !=0){        
+        serial_pc.printf("Failed to read register %#X value during read.\n", address);
+        return status;
+        }
+
+    data = rsp[0];
+    serial_pc.printf("\n\r %#X Status: %#X\n\r", address, rsp[0]);
+    return status;
+}
+
+// Function to write to a registry
+// TODO Check about mfio -> might not need it.
+int MAXM86161::_write_to_reg(int address, int* value) {
+    char cmd[16];
+    char rsp[256];
+    int status;
+    cmd[0] = address; cmd[1] = value;
+    status = i2cbus.write(PPG_ADDR, cmd, 2);
+    if(status !=0) {serial_pc.printf("write error %#x\n", address);}
+    else {
+    // mfio = 1; thread_sleep_for(2); mfio = 0; wait_us(300);
+    // ppg_i2c.read(SH_ADDR, rsp, 1);
+    // mfio = 1; thread_sleep_for(2); mfio = 0; wait_us(300);
+
+    // wait_us(300);
+    i2cbus.read(PPG_ADDR, rsp, 1);
+    // wait_us(300);
+    serial_pc.printf("\n\r %#X Status: %#X\n\r", address, rsp[0]);}
+    return status;
+}
+
+
+
+
+/*
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <stdio.h>
+#include <bitset>
+
+int rsp = 0;
+
+int count = 15;
+int i = 0;
+int main() {
+    // Write C++ code here
+    std::cout << "Hello world!\n\n";
+    
+    for ( i=0; i<count; i++ ){
+        rsp = rsp + 1;
+        std::cout << rsp << "\n";
+    }
+    
+    char a = 255;
+    std::bitset<8> x(a);
+    std::cout << x << '\n';
+    x &= ~(1<<3);
+    // x = x << 1;
+    std::cout << x << "\n";
+
+
+    x |= (1 << 5) | (1 << 7); 
+    std::cout << x << "\n";
+    
+    
+    int c = 0;
+    std::bitset<8> y(c);
+    std::cout << y << '\n';
+    
+    y &= ~(1<<3);
+    // x = x << 1;
+    std::cout << y << "\n";
+
+
+    y |= (1 << 5) | (1 << 7); 
+    std::cout << y << "\n";
+    
+    return 0;
+}
+
+*/
---
+++ MAXM86161.h
@@ -0,0 +1,176 @@
+/**
+ *  Code to get the raw data from a MAX86161
+ *  
+ *  This was written for use with Mbed OS 6.16.0 on a MAX32630FTHR
+ * 
+ * Example of use:
+ * @code
+ * #include "mbed.h"
+ * #include "MAXM86161.h"
+ * #include "USBSerial.h"  // For communication via the onboard USB port
+ * 
+ * 
+ * // Start serial communication
+ * USBSerial serial;
+ * 
+ * // Create the Sensor_Raw object
+ * Sensor_Raw sensor_hub;
+ * 
+ * int main()
+ * {   
+ *     // Initialize the sensor hub with default values
+ *     sensor_hub.init_sh_raw();
+ *     
+ *     // Set the data interrogation rate to 50 Hz.
+ *     sensor_hub.set_sample_rate(50);
+ *     
+ *     // Set the drive current for all 3 LEDs 
+ *     sensor_hub.set_led_current(0x12);
+ *  
+ *     // Variables to hold the ppg data
+ *     int red_ppg, green_ppg, ir_ppg;
+ * 
+ *     while (true) {
+ *       // Read the data at a rate of 50 Hz and output the information via serial
+ *       sensor_hub.read_sh_fifo(&red_ppg, &green_ppg, &ir_ppg);
+ *       serial.printf("%d,%d,%d,\n\r", red_ppg, green_ppg, ir_ppg);
+ *       ThisThread::sleep_for(20ms);
+    }
+ * }
+ * @endcode
+ * 
+*/
+#include "mbed.h"
+#include <stdint.h>
+
+
+class MAXM86161 {
+    public:
+    /** @brief Constructor
+     * @param i2cbus bus for communication via I2C
+     * @param MFIO control pin for the MAX32664
+     * @param RST reset pin to control the MAX32664
+    */
+    MAXM86161(I2C &i2c, PinName MFIO = P5_4, PinName RST = P5_6);
+
+    /** @brief Destructor */
+    ~MAXM86161(void);
+
+    int init(void);
+    int start(void);
+    int stop(void);
+    // void read_fifo(int* red, int* green, int* ir);
+    // void led_off();
+    // void set_led_current(char brightness);
+    // void set_led_red_current(char brightness);
+    // void set_led_green_current(char brightness);
+    // void set_led_ir_current(char brightness);
+    // void set_sample_rate(int rate);
+    // char read_package_temp();
+
+    // Functions not yet working
+    // char read_status();
+
+
+
+
+    private:
+    DigitalInOut mfio;
+    DigitalInOut rst;
+    InterruptIn irq_pin;
+    I2C & i2cbus;
+
+    void _set_led_sequence(char sequence);
+
+    int _read_from_reg(int address);
+    int _write_to_reg(int address, int value);
+    int _get_current_reg_values(char * values);
+
+
+};
+
+
+
+// Define the I2C address of the PPG sensor
+#define PPG_ADDR 0xC4
+
+
+/*******************************************************************************
+ ************************** Maxm86161 I2C Registers *******************************
+ ******************************************************************************/
+
+//Status group
+#define REG_IRQ_STATUS1		0x00
+#define REG_IRQ_STATUS2		0x01
+#define REG_IRQ_ENABLE1		0x02
+#define REG_IRQ_ENABLE2		0x03
+
+//FIFO group
+#define REG_FIFO_WRITE_PTR	  0x04
+#define REG_FIFO_READ_PTR		  0x05
+#define REG_OVF_COUNTER			  0x06
+#define REG_FIFO_DATA_COUNTER	0x07
+#define REG_FIFO_DATA			    0x08
+#define REG_FIFO_CONFIG1		  0x09
+#define REG_FIFO_CONFIG2		  0x0A
+
+//System
+#define REG_SYSTEM_CONTROL		0x0D
+
+//PPG configuration
+#define REG_PPG_SYNC_CONTROL	  0x10
+#define REG_PPG_CONFIG1			    0x11
+#define REG_PPG_CONFIG2			    0x12
+#define REG_PPG_CONFIG3			    0x13
+#define REG_PROX_INT_THRESHOLD	0x14
+#define REG_PD_BIAS				      0x15
+
+//PPG picket fence detect and replace
+#define REG_PICKET_FENCE	0x16
+
+//LEd sequence control
+#define REG_LED_SEQ1		0x20
+#define REG_LED_SEQ2		0x21
+#define REG_LED_SEQ3		0x22
+
+//LED pulse amplitude
+#define REG_LED1_PA			  0x23
+#define REG_LED2_PA			  0x24
+#define REG_LED3_PA			  0x25
+#define REG_LED_PILOT_PA	0x29
+#define REG_LED_RANGE1		0x2A
+
+//PPG1_HI_RES_DAC
+#define REG_S1_HI_RES_DAC1	0x2C
+#define REG_S2_HI_RES_DAC1	0x2D
+#define REG_S3_HI_RES_DAC1	0x2E
+#define REG_S4_HI_RES_DAC1	0x2D
+#define REG_S5_HI_RES_DAC1	0x30
+#define REG_S6_HI_RES_DAC1	0x31
+
+//Die temperature
+#define REG_DIE_TEMP_CONFIG		0x40
+#define REG_DIE_TEMP_INT		  0x41
+#define REG_DIE_TEMP_FRACTION	0x42
+
+//DAC calibration
+#define REG_DAC_CALIB_ENABLE	0x50
+
+//SHA256
+#define REG_SHA_CMD		  0xF0
+#define REG_SHA_CONFIG	0xF1
+
+//Memory
+#define REG_MEMORY_CONTROL	0xF2
+#define REG_MEMORY_INDEX	  0xF3
+#define REG_MEMORY_DATA		  0xF4
+
+// Part ID
+#define REG_REV_ID		0xFE
+#define REG_PART_ID		0xFF
+
+#define REG_FIFO_DATA_MASK  0x07FFFF
+#define REG_FIFO_RES        19
+#define REG_FIFO_TAG_MASK   0x1F
+
+#endif
